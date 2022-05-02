33294b06c16e2e09c37c3022536abd3824c180d4
JanVasilcenko <jan.vasilcenko@gnj.cz>
2022-05-02 11:39:42 +0200

    Project setup

---
+++ src/main/java/com/group31/bestmovies/BestMoviesApplication.java
@@ -0,0 +1,15 @@
+package com.group31.bestmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@SpringBootApplication
+public class BestMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestMoviesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group31/bestmovies/BestMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group31.bestmovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
