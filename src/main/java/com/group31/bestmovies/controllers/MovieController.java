bc92693be31135021ffe42fd5973992dfc6d6da7
JanVasilcenko <jan.vasilcenko@gnj.cz>
2022-05-15 20:02:10 +0200

    test api

--- src/main/java/com/group31/bestmovies/controllers/MovieController.java
+++ src/main/java/com/group31/bestmovies/controllers/MovieController.java
@@ -4,10 +4,12 @@ import com.group31.bestmovies.api.MovieApi;
 import com.group31.bestmovies.api.MoviesApi;
 import com.group31.bestmovies.api.UserApi;
 import com.group31.bestmovies.models.*;
+import org.checkerframework.checker.units.qual.A;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -26,7 +28,13 @@ public class MovieController implements MovieApi, MoviesApi {
 
     @Override
     public ResponseEntity<List<Movie>> getMyMovieList() {
-        return null;
+        Movie newMovie = new Movie();
+        newMovie.setMovieID(1);
+        newMovie.setTitle("Indiana Jones");
+        newMovie.setYear(1875);
+        ArrayList arrayList = new ArrayList();
+        arrayList.add(newMovie);
+        return new ResponseEntity<>(arrayList,HttpStatus.OK);
     }
 
     @Override
