c1b50f1a0ce506504c7c7fdb4019ef01cbf0c446
popalnicolas <56255509+popalnicolas@users.noreply.github.com>
2022-05-19 19:41:28 +0200

    Create CorsConfig.java

---
+++ src/main/java/com/group31/bestmovies/Security/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.group31.bestmovies.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer getCorsConfiguration()
+    {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("POST", "GET", "DELETE", "PUT")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
