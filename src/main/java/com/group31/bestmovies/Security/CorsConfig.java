2643c0b3864428f6e96fa590f469976495375ddc
popalnicolas <56255509+popalnicolas@users.noreply.github.com>
2022-05-19 19:58:13 +0200

    Update CorsConfig.java

--- src/main/java/com/group31/bestmovies/Security/CorsConfig.java
+++ src/main/java/com/group31/bestmovies/Security/CorsConfig.java
@@ -1,6 +1,7 @@
 package com.group31.bestmovies.Security;
 
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -9,6 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig extends WebMvcAutoConfiguration implements WebMvcConfigurer{
     
+    @Bean
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
