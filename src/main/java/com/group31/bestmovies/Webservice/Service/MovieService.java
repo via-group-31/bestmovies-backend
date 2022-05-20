fa33577e206253a51dc759e51735947df5d0f759
popalnicolas <popal.nicolas@gmail.com>
2022-05-20 16:07:55 +0200

    Poster implementation to all endpoints with movies

--- src/main/java/com/group31/bestmovies/Webservice/Service/MovieService.java
+++ src/main/java/com/group31/bestmovies/Webservice/Service/MovieService.java
@@ -22,39 +22,49 @@ public class MovieService {
     private final IMovieRepository movieRepository;
 
     public List<Movie> getMoviesWithSimilarName(String name) {
-        return movieRepository.getMoviesWithSimilarName(name);
+        List<Movie> movies = movieRepository.getMoviesWithSimilarName(name);
+        for(Movie movie: movies){
+            movie.setMoviePoster(getMoviePosters(movie));
+        }
+        return movies;
     }
 
     public Movie getMovieById(long movieId) {
-        return movieRepository.getById(movieId);
+        Movie movie = movieRepository.getById(movieId);
+        movie.setMoviePoster(getMoviePosters(movie));
+        return movie;
     }
 
     public List<Movie> getMovies() {
         List<Movie> movies = movieRepository.getMovies();
 
-        RestTemplate restTemplate = new RestTemplate();
-
         for(Movie movie: movies){
-            String movieId = String.format("%07d", movie.getMovieId());
-
-            String uri = "https://www.omdbapi.com/?apikey=c3d1f671&i=tt" + movieId;
-            String imdb = restTemplate.getForObject(uri, String.class);
-
-            final String regex = ",\"Poster\":\"(.*?)\"";
-            final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
-            final Matcher matcher = pattern.matcher(imdb);
-
-            String poster = "";
-
-            while (matcher.find()) {
-                for (int i = 1; i <= matcher.groupCount(); i++) {
-                    poster = matcher.group(1);
-                }
-            }
-
-            movie.setMoviePoster(poster);
+            movie.setMoviePoster(getMoviePosters(movie));
         }
 
         return movies;
+    }
+
+    private String getMoviePosters(Movie movie){
+        RestTemplate restTemplate = new RestTemplate();
+
+        String movieId = String.format("%07d", movie.getMovieId());
+
+        String uri = "https://www.omdbapi.com/?apikey=c3d1f671&i=tt" + movieId;
+        String imdb = restTemplate.getForObject(uri, String.class);
+
+        final String regex = ",\"Poster\":\"(.*?)\"";
+        final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+        final Matcher matcher = pattern.matcher(imdb);
+
+        String poster = "";
+
+        while (matcher.find()) {
+            for (int i = 1; i <= matcher.groupCount(); i++) {
+                poster = matcher.group(1);
+            }
+        }
+
+        return poster;
     }
 }
