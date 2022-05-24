3115fb218c2f93cc0ae345948b401ec119048813
popalnicolas <56255509+popalnicolas@users.noreply.github.com>
2022-05-24 17:54:43 +0200

    Update ReviewController.java

--- src/main/java/com/group31/bestmovies/Webservice/Api/ReviewController.java
+++ src/main/java/com/group31/bestmovies/Webservice/Api/ReviewController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/review")
